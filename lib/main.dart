1f4bb56813562df9da5c6c3eec5a12ae681334dc
HirotoKonno <great.hiroto@gmail.com>
2023-11-24 20:20:02 +0900

    Dix import

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '144_Animate/animate_view.dart';
-import '146_Draggable/draggable_view.dart';
-import '147_CallbackShortcuts/callback_shortcuts_view.dart';
-import '153_FlChart/fl_chart_view.dart';
 import '154_Gap/gap_view.dart';
 
 void main() {
